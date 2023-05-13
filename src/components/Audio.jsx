2a6cbe76a6a0a5dee01326ad9bdd14060c8d0995
matthieu-scub <matthieu.dejean@scub.net>
2023-05-13 21:26:20 +0200

    Enchainement des fichiers audio

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,20 +1,26 @@
 import { onMount } from "solid-js";
+import { createSignal } from "solid-js";
 
-export default function Audio() {
+export default function Audio(props) {
   let audio;
+  const { src, interval } = props;
+  const [playing, setPlaying] = createSignal(false);
+
   onMount(() => {
-    // audio.playbackRate = 1.25;
+    audio.pause();
+    setTimeout(() => {
+      audio.play();
+      // audio.playbackRate = 1.25;
+      // Si j'applique le playbackRate, pensez à changer le temps du delay des endelay dans le fichier index
+      // et aussi de mettre à jour l'interval de tous les fichiers audio
+      // diviser par 1.25 pour avoir le bon interval
+      setPlaying(true);
+    }, interval);
   });
+
   return (
     <>
-      <audio
-        ref={audio}
-        controls
-        autoplay
-        src="Contexte.m4a"
-        className="absolute top-0"
-      />
-      ;
+      <audio ref={audio} autoPlay src={src} className="absolute top-0" />
     </>
   );
 }
